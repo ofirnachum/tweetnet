16b1fd477d780dbdbff7958a290893798f0d5e88
Louis Sobel <sobel@mit.edu>
2014-04-18 16:55:48 -0400

    botmaster automatic

--- stupid_botmaster.py
+++ stupid_botmaster.py
@@ -2,6 +2,7 @@
 example of a stupid botmaster
 """
 import sys
+import time
 
 from tweetnet import Tweetnet
 
@@ -11,4 +12,17 @@ if __name__ == "__main__":
 
     api = Tweetnet(round_id, role='admin')
 
-    api.tweet(user, "#wow %s" % sys.argv[3])
+    last_check = 0
+    already = set()
+
+    while True:
+        print "Checking for new flags..."
+        flags = api.get_flags(since=last_check)
+        last_check = int(time.time()) - 1
+        if flags:
+            for flag in flags:
+                print "found one: %s" % flag['flag_id']
+                print "tweeting '#wow %s'" % flag['flag_id']
+                
+            api.tweet(user, "#wow %s" % flag['flag_id'])
+        time.sleep(3)
