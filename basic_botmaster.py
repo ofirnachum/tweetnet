9114aea06382ed614a07c8e739c400ef86822758
Ofir Nachum <ofir@mit.edu>
2014-04-24 17:01:07 -0400

    tweet queue for master

--- basic_botmaster.py
+++ basic_botmaster.py
@@ -6,46 +6,71 @@ import botnet_utils as utils
 
 from tweetnet import Tweetnet
 
+TWEET_PERIOD = 1;
+SLEEP_PERIOD = 1;
+
+class TweetQueue:
+    # NOTE: Tweet frequency is relative
+    # to how often you call 'pop'.
+    def __init__(self, user, api, tweet_period):
+        self.user = user;
+        self.api = api;
+        self.tweet_period = tweet_period;
+        self.queue = [];
+
+    def push(self, content):
+        self.queue.append(content);
+
+    def pop(self):
+        # Successful pop happens
+        # with probability 1/tweet_period
+        if (self.queue and (random.random() * self.tweet_period < 1)):
+            content = self.queue.pop(0);
+            self.api.tweet(self.user, content);
+            return content;
+        return False;
+
+
+
 def get_random_tweet(api):
     """
     returns random tweet from all tweets
     """
-    tweet = api.get_realistic_tweet();
+    tweet = "hello hello";#api.get_realistic_tweet();
     return tweet;
 
 def get_random_short_tweet(api, max_len):
     """
     returns random tweet of at most max_len length from all tweets
     """
-    tweet = api.get_realistic_tweet();
+    tweet = get_random_tweet(api);
     while (len(tweet) > max_len):
-        tweet = api.get_realistic_tweet();
+        tweet = get_random_tweet(api);
     return tweet;
 
-def tweet_flag_component(api, comp):
+def tweet_flag_component(api, queue, comp):
     random_content = get_random_short_tweet(api, 138).strip();
     tweet_content = random_content + utils.to_punctuation(comp) + ' ';
-    api.tweet(user, tweet_content);
+    queue.push(tweet_content);
 
-def tweet_flag(api, flag):
+def tweet_flag(api, queue, flag):
     # Signal beginning of flag
     random_content = get_random_short_tweet(api, 138);
-    api.tweet(user, random_content + "  ");  
+    queue.push(random_content + "  ");  
     # Tweet flag in chunks of base-4 digits
     for xy in flag['flag_id']:
         n = int(xy, 16);
         x = n // 4;
         y = n % 4;
-        tweet_flag_component(api, x);
-        time.sleep(1);
-        tweet_flag_component(api, y);
-        time.sleep(1);
+        tweet_flag_component(api, queue, x);
+        tweet_flag_component(api, queue, y);
 
 if __name__ == "__main__":
     round_id = sys.argv[1]
     user = sys.argv[2]
 
     api = Tweetnet(round_id, role='admin')
+    queue = TweetQueue(user, api, TWEET_PERIOD);
 
     # Last check for flags
     last_check = 0
@@ -64,6 +89,6 @@ if __name__ == "__main__":
             api.tweet(user, random_tweet.strip());
         if flags:
             for flag in flags:
-                tweet_flag(api, flag);
-                time.sleep(10);
-        time.sleep(3)
+                tweet_flag(api, queue, flag);
+        queue.pop();
+        time.sleep(SLEEP_PERIOD)
