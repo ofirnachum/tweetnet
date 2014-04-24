0d5c092d286de242d4230c621e47cece79fd7d5b
Joshua Blum <joshblum@mit.edu>
2014-04-24 17:08:12 -0400

    btly req, count bug

--- botmaster.py
+++ botmaster.py
@@ -5,11 +5,15 @@ class SingleCharBotMaster(BotMaster):
 
     def submit_flag(self, flag_id):
         sent_char = 0
-        rand_num = self.prng.next()  # every how many tweets?
+        rand_num = self.prng.next()
+        count = rand_num
         while sent_char < len(flag_id):
             tweet = self.api.get_realistic_tweet()
-            if rand_num == 0:
+            if count != 0:
+                count -= 1
+            else:
                 rand_num = self.prng.next()
+                count = rand_num
                 tweet = self.insert_char(rand_num,
                                          flag_id[sent_char], tweet)
                 sent_char += 1
@@ -18,7 +22,7 @@ class SingleCharBotMaster(BotMaster):
 
     def insert_char(self, rand_num, char, tweet):
         content = list(tweet['content'])
-        rand_num %= len(content)
-        content[rand_num] = char  # replace w/char
+        index = rand_num % len(content)
+        content[index] = char  # replace w/char
         tweet['content'] = content
         return tweet
