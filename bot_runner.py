36a958f6cb3fd2b40bb83b1e82afd451ea1a657e
Louis Sobel <sobel@mit.edu>
2014-04-27 11:14:18 -0400

    dev api and api types

--- bot_runner.py
+++ bot_runner.py
@@ -1,6 +1,6 @@
 import sys
 
-from tweetnet import Tweetnet
+from tweetnet import get_api_type
 from bots import get_bot_type
 
 if __name__ == "__main__":
@@ -9,7 +9,13 @@ if __name__ == "__main__":
     bot_type = sys.argv[3]
     botmaster = sys.argv[4]
 
-    api = Tweetnet(round_id, role='bot')
+    try:
+        api_type = sys.argv[5]
+    except IndexError:
+        api_type = 'default'
+    api_obj = get_api_type(api_type)
+    api = api_obj(round_id, role='bot')
+
     bot_obj = get_bot_type(bot_type)
     bot = bot_obj(bot_id, api, botmaster)
 
--- botmaster_runner.py
+++ botmaster_runner.py
@@ -10,7 +10,13 @@ if __name__ == "__main__":
     round_id = sys.argv[1]
     user = sys.argv[2]
     bot_type = sys.argv[3]
-    api = Tweetnet(round_id, role='admin')
+
+    try:
+        api_type = sys.argv[4]
+    except IndexError:
+        api_type = 'default'
+    api_obj = get_api_type(api_type)
+    api = api_obj(round_id, role='bot')
 
     master_obj = get_bot_type(bot_type, is_master=True)
     master = master_obj(user, api)
